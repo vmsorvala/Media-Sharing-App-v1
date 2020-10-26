7104ec739da3c7b1c34ef15464fc9f97c55f78b8
vmsorvala <vmsorvala@gmail.com>
2020-10-26 22:42:19 +0200

    week2: routing 1 ja 2

--- week2/app.js
+++ week2/app.js
@@ -7,4 +7,18 @@ app.get('/cat', (req, res) => {
   res.send('From this endpoint you can get cats.')
 });
 
+app.post('/cat', (req, res) => {
+  res.send('With this endpoint you can add cats.')
+});
+
+app.put('/cat', (req, res) => {
+  res.send('With this endpoint you can edit cats.')
+});
+
+app.delete('/cat', (req, res) => {
+  res.send('With this endpoint you can delete cats.')
+});
+
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
