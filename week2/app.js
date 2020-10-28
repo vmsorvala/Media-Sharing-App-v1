e9492b9030f77b63941f0f16e235b9b1a10f8a8b
vmsorvala <vmsorvala@gmail.com>
2020-10-28 19:08:27 +0200

    week2 Middleware kohdat 1. ja 2.

--- week2/app.js
+++ week2/app.js
@@ -7,7 +7,7 @@ const port = 3000;
 var catRouter = require('./routes/catRoute')
 var userRouter = require('./routes/userRoute')
 
-//app.use(cors())
+app.use(cors())
 app.use(catRouter)
 app.use(userRouter)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
