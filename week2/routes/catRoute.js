37321e29801750259283f99cde3fa2c3c65bf0d0
vmsorvala <vmsorvala@gmail.com>
2020-10-29 19:12:31 +0200

    week2 Middleware kohta 4. ja 5.

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -3,7 +3,21 @@
 var express = require('express')
 var router = express.Router()
 const catController = require('../controllers/catController');
+const multer = require('multer') // v1.0.5
+//const upload = multer({ dest: 'uploads/' }) // for parsing multipart/form-data
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+
+var upload = multer({ storage: storage })
 
 router.get('/cat',  catController.cat_list_get);
 
@@ -11,8 +25,9 @@ router.get('/cat/:id',(req, res) => {
 catController.cat_get(req,res, req.params.id)}
 );
 
-router.post('/cat', (req, res) => {
-  res.send('With this endpoint you can add cats.')
+router.post('/cat', upload.single('cat'), function (req, res, next) {
+    console.log(req.body )
+    res.json(req.body)
 });
 
 router.put('/cat', (req, res) => {
