167f8ea997eea586a4c2841c7de0c9d1d05f6ce5
vmsorvala <vmsorvala@gmail.com>
2020-10-29 21:07:54 +0200

    database branch v0

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -5,6 +5,11 @@ const catModel = require('../models/catModel');
 
 const cats = catModel.cats;
 
+const cat_list_get = async (req, res) => {
+  const cats = await catModel.getAllCats();
+  res.json(cats);
+};
+
 const cat_list_get = (req, res) => {
   res.json(cats);
 };
--- week2/models/catModel.js
+++ week2/models/catModel.js
@@ -1,23 +1,28 @@
 'use strict';
-const cats = [
-  {
-    id: '1',
-    name: 'Frank',
-    age: '6',
-    weight: '5',
-    owner: '1',
-    filename: 'http://placekitten.com/400/300',
-  },
-  {
-    id: '2',
-    name: 'James',
-    age: '4',
-    weight: '11',
-    owner: '2',
-    filename: 'http://placekitten.com/400/302',
-  },
-];
+'use strict';
+const pool = require('../database/db');
+const promisePool = pool.promise();
+
+const getAllCats = async () => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner name too.
+    const [rows] = await promisePool.query('SELECT * FROM wop_cat');
+    return rows;
+  } catch (e) {
+    console.log('error', e.message);
+  }   
+};
+
+const getcat = async () => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner name too.
+    const [rows] = await promisePool.execute('SELECT * FROM wop_cat WHERE cat_id = X');
+    return rows;
+  } catch (e) {
+    console.log('error', e.message);
+  }   
+};
 
 module.exports = {
-  cats,
+  getAllCats, getCat,
 };
